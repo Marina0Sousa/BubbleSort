437f04f452b202b09a57b97d0d9e72d06dd7cf8a
Marina Bianca Freitas de Sousa <marinafreitasdesousa@gmail.com>
2025-04-17 00:37:11 +0000

    exercicio bubble sort

---
+++ exercicio.js
@@ -0,0 +1,12 @@
+let dados = [97, 45, 49, 32, 2, 5, 9, 99, 1, 7];
+
+// perccorrer todos os itens do array.
+for (let i = 0; i < dados.length; i++) {
+    console.log("O índice é: " + i + " o valor é: " + dados [i] );
+}
+
+// paa cada item, verificar se ele é maior que o próximo. 
+
+// Se for, inverter todos os valores.
+
+// Caso contrário, não fazer nada e avançar para o próximo.
